6232db8a486702ff5b49ba6542dd14b56599a330
Shreeraksha <shreeraksharaithal.181co149@nitk.edu.in>
2020-09-24 21:17:06 +0530

    Added code for day 1

---
+++ Day 01/Minimum Swaps to Make Strings Equal.cpp
@@ -0,0 +1,36 @@
+/*
+Author : Shreeraksha R Aithal
+Problem name : Minimum Swaps to Make Strings Equal
+Problem statement : https://leetcode.com/problems/minimum-swaps-to-make-strings-equal/
+Difficulty : Medium
+Tags : Greedy, String
+*/
+
+class Solution {
+public:
+    int minimumSwap(string s1, string s2) {
+        int i,ix=-1,iy=-1,n = s1.size(),cnt=0;
+        for(i=0;i<n;i++){
+            if(s1[i] == s2[i])  continue;
+            else{
+                if(s1[i] == 'x'){
+                    if(ix != -1){
+                        cnt++;
+                        ix = -1;
+                    }
+                    else    ix = i;
+                }
+                else{
+                    if(iy != -1){
+                        cnt++;
+                        iy = -1;
+                    }
+                    else    iy = i;                    
+                }
+            }
+        }
+        if(ix>=0 && iy>=0)    cnt+=2;
+        else if(ix>=0 || iy>=0)   return -1;
+        return cnt;
+    }
+};
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# 25DaysOfCP
-My commitment for 25 days Competitive Programming (might extend it later :P )
+# 25 Days Of CP
+My commitment for 25 days of Competitive Programming (might extend it later :P )
+
+## Question bank
+Day 1 : [Sort Colors](https://leetcode.com/problems/sort-colors/) | [Minimum Swaps to Make Strings Equal](https://leetcode.com/problems/minimum-swaps-to-make-strings-equal/)
