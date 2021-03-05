81cd552dd7726873b547815064c0dbc7ea116bd7
Shreeraksha <shreeraksharaithal.181co149@nitk.edu.in>
2021-03-05 18:57:55 +0530

    Added solutions for Day 5

---
+++ Day 05/Binary Tree Level Order Traversal.cpp
@@ -0,0 +1,45 @@
+/*
+Author : Shreeraksha R Aithal
+Problem name : Binary Tree Level Order Traversal
+Problem link : https://leetcode.com/problems/binary-tree-level-order-traversal/
+Difficulty : Medium
+Tags : Breadth-first Search, Tree
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> answer;
+        if(root == nullptr)     return answer;
+        vector<int> v(1,1);
+        queue<pair<TreeNode*,int>> q;
+        int level = 0;
+        q.push({root, 0});
+        while(!q.empty()){
+            root = q.front().first;
+            level = q.front().second;
+            q.pop();
+            if(answer.size() == level){
+                v[0] = root->val;
+                answer.push_back(v);
+            }
+            else{
+                answer[level].push_back(root->val);
+            }
+            if(root->left !=nullptr)    q.push({root->left, level+1});
+            if(root->right !=nullptr)    q.push({root->right, level+1});
+        }
+        return answer;
+    }
+};
--- README.md
+++ README.md
@@ -6,3 +6,4 @@ My commitment for Competitive Programming during March 2021.
 - Day 2 : [Partition Labels](https://leetcode.com/problems/partition-labels/) | [Push Dominoes](https://leetcode.com/problems/push-dominoes/)
 - Day 3 : [Bulb Switcher](https://leetcode.com/problems/bulb-switcher/) | [Jump Game](https://leetcode.com/problems/jump-game/)
 - Day 4 : [Intersection of Two Linked Lists](https://leetcode.com/problems/intersection-of-two-linked-lists/) | [Reverse Words in a String](https://leetcode.com/problems/reverse-words-in-a-string/)
+- Day 5 : [Binary Tree Level Order Traversal](https://leetcode.com/problems/binary-tree-level-order-traversal/) | [Binary Tree Level Order Traversal II](https://leetcode.com/problems/binary-tree-level-order-traversal-ii/)
